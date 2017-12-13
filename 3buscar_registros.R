5e68000846299f8c629e24838e6134d8ea0f9c3a
Andrea Sánchez Tapia <katori@gmail.com>
2017-12-13 14:55:22 -0500

    download datos especies

--- 3buscar_registros.R
+++ 3buscar_registros.R
@@ -1,36 +1,47 @@
-#setwd("/Users/Sanchez/Documents/2 Coleguinhas/Mariana/Iguatemy/")
 library(dplyr)
 spp <- read.csv("./output/gettaxaresults.csv", row.names = 1)
 library(rgbif)
-help("rgbif")
-spp <- spp %>% filter(!is.na(id)) %>% filter(no_authors!= "?")
-dim(spp)
-nombres <- unique(droplevels(spp$no_authors))
-    occs <- occ_search(nombres)
-key <- vector(length= length(nombres))
 
+spp <- spp %>% filter(!is.na(id)) %>% filter(no_authors != "?")
+dim(spp)
+nombres <- sort(unique(droplevels(spp$no_authors)))
+
+key <- vector(length = length(nombres))
 
 occs <- list()
-for (i in c(1:length(nombres)))) {
-	key <- name_suggest(q = nombres[i], rank='species')$key[1]
-    occs[[i]] <- occ_search(taxonKey = key, limit = 100000, hasCoordinate = TRUE, basisOfRecord = "PRESERVED_SPECIMEN", hasGeospatialIssue = F)
-  write.csv(occs[[i]])
+
+write.table(nombres,"./data/nombres.txt")
+nombres_processed <- read.table("./data/nombres_processed.txt")
+head(nombres_processed)
+nombres <- sort(unique(nombres_processed$V2))
+
+i
+length(nombres)
+for (i in c(701:742)) {
+print(nombres[i])
+	key <- name_suggest(q = nombres[i], rank = 'species')$key[1]
+	print(key)
+	if (!is.null(key)){
+  occs[[i]] <- occ_search(taxonKey = key,
+			limit = 100000,
+			                       hasCoordinate = TRUE,
+                       basisOfRecord = "PRESERVED_SPECIMEN",
+                       hasGeospatialIssue = F,
+                       return = 'data',
+											 fields = "minimal")
+
+	print(dim(occs[[i]]))
+if (!is.null(dim(occs[[i]]))) {
+	write.csv(x = data.frame(occs[[i]]), file = paste0("./data/occs/",nombres[i],".csv"))
+cat(paste(nombres[i], "DONE","\n"))
+}
 }
 
-	help(rgbif)
-     occs[[i]] <- occs[[i]][!(is.na(occs[[i]]$decimalLatitude)), ]
-# }
-#
+}
+list.files("./data/occs")
 # gbif_all <- rbindlist(l = occs, use.names = T, fill = T, idcol = "id")
 # head(gbif_all)
 # dim(gbif_all)
-splist <- c('Cyanocitta stelleri', 'Junco hyemalis', 'Aix sponsa')
-keys <- sapply(splist, function(x) name_suggest(x)$key[1], USE.NAMES= FALSE)
-occ_data()
-
-
-hasCoordinate=TRUE,
-hasGeospatialIssue= F,
 
 
 key <- vector(length = length(spp$id))
@@ -45,4 +56,3 @@ occs <- list()
 # dim(gbif_all)
 #
 # write.csv(gbif_all, "./data/rgbif/GBIFALL.csv")
-	
