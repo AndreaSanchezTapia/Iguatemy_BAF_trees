28950226c57fa289e54ea2bdf7dff75b3d0314fc
Andrea Sánchez Tapia <katori@gmail.com>
2018-02-24 14:35:13 -0300

    + Cambié el script de búsqueda de ocurrencias para que mejorara la búsqueda por keys. + Lo ejecuté hasta Ficus eximia 101-200 + [ö]seguir ejecutando

--- 3buscar_registros.R
+++ 3buscar_registros.R
@@ -8,77 +8,47 @@ nombres <- sort(unique(droplevels(spp$no_authors)))
 
 key <- vector(length = length(nombres))
 
-occs <- list()
 write.table(nombres, "./data/nombres.txt")
 #aqui fiz a mão para tirar famílias, gëneros, sp. aff. e cf.
-nombres_processed <- read.table("./data/nombres_processed.txt")
+nombres_processed <- read.table("./data/nombres/nombres_processed.txt")
 head(nombres_processed)
 nombres <- sort(unique(nombres_processed$V2))
 
+
 length(nombres)
-#aqui deve ser 1 a 742 mas fiz por levas
-for (i in c(1:742)) {
+#aqui deve ser 1 a 742 mas fiz por levas - modifico para que cate todos os keys e elimine duplicados:
+for (i in c(208:300)) {
     print(nombres[i])
-    key <- name_suggest(q = nombres[i], rank = 'species')$key[1]
+    key <- name_suggest(q = nombres[i], rank = 'species')$key
+
     print(key)
     if (!is.null(key)) {
-        occs[[i]] <- occ_search(
-            taxonKey = key,
-            limit = 100000,
-            hasCoordinate = TRUE,
-            basisOfRecord = "PRESERVED_SPECIMEN",
-            hasGeospatialIssue = F,
-            return = 'data',
-            fields = "minimal"
-        )
-        print(dim(occs[[i]]))
-        if (!is.null(dim(occs[[i]]))) {
+        occs <- list()
+        for (k in 1:length(key)) {
+            occs[[k]] <- occ_search(
+                taxonKey = key[k],
+                limit = 100000,
+                hasCoordinate = TRUE,
+                basisOfRecord = "PRESERVED_SPECIMEN",
+                hasGeospatialIssue = F,
+                return = 'data',
+                fields = "minimal"
+            )
+        }
+    }
+
+        print(lapply(occs,dim))
+        if (any(!is.null(lapply(occs,dim)))) {
+            dim.null <- lapply(occs, function(x) {!is.null(dim(x))})
+            occs.f <- subset(occs, dim.null == T)
+            occs.f <- bind_rows(occs.f)
+            occs.f <- occs.f[!duplicated(occs.f[,c(1,3,4)]),]
+            print(dim(occs.f))
             write.csv(
-                x = data.frame(occs[[i]]),
+                x = data.frame(occs.f),
                 file = paste0("./data/occs/", nombres[i], ".csv")
             )
             cat(paste(nombres[i], "DONE", "\n"))
         }
     }
-}
-
-
-##busqueda uno por uno de cosas que faltaron
-nombre <- "Xylosma glaberrima"
-
-#
-    print(nombre)
-    key <- name_suggest(q = nombre, rank = 'species')$key
-        #key <- name_suggest(q = nombre, rank = 'species')$key[2]
-        #key <- name_suggest(q = nombre, rank = 'species')$key[3]
-    print(key)
-    occs <- list()
-    for (l in 1:length(key)) {
-
-        occs[[l]] <- occ_search(
-            taxonKey = key[l],
-            limit = 100000,
-            hasCoordinate = TRUE,
-            basisOfRecord = "PRESERVED_SPECIMEN",
-            hasGeospatialIssue = F,
-            return = 'data',
-            fields = "minimal"
-        )
-    }
-
-        lapply(occs,dim)
-occs.f <- occs[[1]]
-#occs.f <- occs[[2]]
-#occs.f <- occs[[3]]
- #       occs.f <- bind_rows(occs[[2]],occs[[3]])
-  #      occs.f <- bind_rows(occs[[3]],occs[[4]])
-   #     occs.f <- bind_rows(occs[[1]],occs[[3]],occs[[4]], occs[[5]])
-    occs.f <- occs.f[!duplicated(occs.f[,c(1,3,4)]),]
-dim(occs.f)
-            if (!is.null(dim(occs.f))) {
-            write.csv(
-                x = data.frame(occs.f),
-                file = paste0("./data/occs/", nombre, ".csv"))
-            cat(paste(nombre, "DONE", "\n"))
-        }
 
